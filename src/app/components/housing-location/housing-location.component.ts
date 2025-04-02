3024932e98e45a6be018cbf3d563a0a2c4c95ac7
Zarab <santiagozarabanda8a@gmail.com>
2025-04-02 00:12:30 -0500

    routes

--- src/app/components/housing-location/housing-location.component.ts
+++ src/app/components/housing-location/housing-location.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Housinglocation } from '../housinglocation';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -9,5 +9,5 @@ import { Housinglocation } from '../housinglocation';
 })
 export class HousingLocationComponent {
 
-  @Input() housingLocation!:Housinglocation;
+  @Input() housingLocation!:HousingLocation;
 }
